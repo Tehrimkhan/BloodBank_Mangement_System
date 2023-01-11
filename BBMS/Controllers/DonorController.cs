bbe8bc56bd63a7e905d5940b93c887ac44a25fab
Taimoor Shoaib <TaimoorShoaib@gmail.com>
2023-01-11 22:35:06 +0500

    BBMS commit

---
+++ BBMS/Controllers/DonorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BBMS.Models;
+
+
+namespace BBMS.Controllers
+{
+    public class DonorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Donor()
+        {
+            Donor donorModel = new Donor();
+            return View(donorModel);
+        }
+        [HttpPost]
+        public ActionResult Donor(Donor donorModel)
+        {
+            using (DbmsEntities1 dbModel = new DbmsEntities1())
+            {
+                dbModel.Donors.Add(donorModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Succesfull";
+            return View("Donor", new Donor());
+        }
+    }
+}
---
+++ BBMS/Controllers/RecieverController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BBMS.Models;
+
+
+namespace BBMS.Controllers
+{
+    public class RecieverController : Controller
+    {
+        [HttpGet]
+        public ActionResult Reciever()
+        {
+            Reciever recieverModel = new Reciever();
+            return View(recieverModel);
+        }
+        [HttpPost]
+        public ActionResult Reciever(Reciever recieverModel)
+        {
+            using (DbmsEntities2 dbModel = new DbmsEntities2())
+            {
+                dbModel.Recievers.Add(recieverModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Succesfull";
+            return View("Reciever", new Reciever());
+        }
+    }
+}
